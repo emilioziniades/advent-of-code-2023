fc79498458f05433ddb0c9b431f2bc100db65ae4
Emilio Ziniades <emilioziniades@protonmail.com>
2023-11-27 23:10:03 +0200

    fetches things from AOC!

--- src/Fetch.hs
+++ src/Fetch.hs
@@ -2,16 +2,22 @@
 
 module Fetch (fetchInput) where
 
+import Configuration.Dotenv
 import Control.Monad.IO.Class
+import Data.String (IsString (fromString))
 import Network.HTTP.Req
+import System.Environment (getEnv)
 
 fetchInput :: Int -> IO ()
 fetchInput day = runReq defaultHttpConfig $ do
+    loadFile defaultConfig
+    token <- liftIO $ getEnv "AOC_TOKEN"
     r <-
         req
             GET
             (https "adventofcode.com" /: "2022" /: "day" /~ day /: "input")
             NoReqBody
             bsResponse
-            mempty
+            (header "Cookie" $ fromString $ "session=" ++ token)
+
     liftIO $ print (responseBody r)
