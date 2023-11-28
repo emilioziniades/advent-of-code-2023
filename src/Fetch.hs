af95431f72714b59717d27455b2656e97acdf333
Emilio Ziniades <emilioziniades@protonmail.com>
2023-11-28 15:16:20 +0200

    Fetch module complete!

--- src/Fetch.hs
+++ src/Fetch.hs
@@ -1,21 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Fetch (fetchInput) where
+module Fetch (getInput) where
 
 import Configuration.Dotenv
+import Control.Monad
 import Control.Monad.IO.Class
 import qualified Data.ByteString as BS
 import Data.String (IsString (fromString))
 import Network.HTTP.Req
+import System.Directory
 import System.Environment (getEnv)
+import Text.Printf
 
 type Day = Int
 
-fetchInput :: Day -> IO ()
-fetchInput day = runReq defaultHttpConfig $ do
+getInput :: Day -> IO ()
+getInput day = do
+    fileExists <- doesFileExist (inputFilename day)
+    unless fileExists (downloadInput day)
+
+downloadInput :: Day -> IO ()
+downloadInput day = runReq defaultHttpConfig $ do
     loadFile defaultConfig
     token <- liftIO $ getEnv "AOC_TOKEN"
     let headers = header "Cookie" $ fromString $ "session=" ++ token
     let url = https "adventofcode.com" /: "2022" /: "day" /~ day /: "input"
     r <- req GET url NoReqBody bsResponse headers
-    liftIO $ BS.writeFile "hellyeah.txt" $ responseBody r
+    liftIO $ BS.writeFile (inputFilename day) (responseBody r)
+
+inputFilename :: Day -> String
+inputFilename day = "input/" ++ printf "day%02d.txt" day
