e35dca57399b57f94339311dd0b597da4e63bfba
Emilio Ziniades <emilioziniades@protonmail.com>
2023-12-03 18:22:20 +0200

    day 3 refactor

--- src/Day03.hs
+++ src/Day03.hs
@@ -12,47 +12,59 @@ data Range = Range Point Point
 type Grid = [[Char]]
 type Row = [Char]
 
+data PartNumber = PartNumber {number :: Int, _range :: Range}
+
+-- Part 1
+
 sumPartNumbers :: String -> IO Int
 sumPartNumbers filename = do
     file <- readFile filename
     let array = words file
-    pure $ sum $ fst <$> filter (isPartNumber array) (getNumbers array)
+    pure $ sum $ number <$> filter (isPartNumber array) (getPartNumbers array)
 
-getNumbers :: Grid -> [(Int, Range)]
-getNumbers array =
+getPartNumbers :: Grid -> [PartNumber]
+getPartNumbers array =
     let
-        makeNumbers (row, x) = getNumberInRow x row
+        makeNumbers (row, x) = getPartNumbersInRow x row
      in
         mconcat $ fmap makeNumbers (enumerate array)
 
-getNumberInRow :: Int -> Row -> [(Int, Range)]
-getNumberInRow x row =
+getPartNumbersInRow :: Int -> Row -> [PartNumber]
+getPartNumbersInRow x row =
     let
-        gRow = groupBy bothNumbers (enumerate row)
-        numbers = filter (isNumber . fst . head) gRow
-        bothNumbers (a, _) (b, _) = isNumber a && isNumber b
-        getRange :: [(Char, Int)] -> Range
-        getRange digits = Range (Point x ((snd . head) digits)) (Point x ((snd . last) digits))
-        getNumber :: [(Char, Int)] -> Int
-        getNumber digits = read $ fst <$> digits
-        makeNumbers number = (getNumber number, getRange number)
+        groups = groupBy areBothNumbers (enumerate row)
+        numbers = filter (isNumber . fst . head) groups
      in
-        fmap makeNumbers numbers
+        fmap (makePartNumber x) numbers
 
-isPartNumber :: Grid -> (Int, Range) -> Bool
-isPartNumber array (_, range) =
-    let
-        rangeNeighbours = getRangeNeighbours array range
-        isSymbol c = not (isNumber c) && c /= '.'
-     in
-        any isSymbol rangeNeighbours
+makeRange :: Int -> [(Char, Int)] -> Range
+makeRange x ns = Range (Point x ((snd . head) ns)) (Point x ((snd . last) ns))
 
-getRangeNeighbours :: Grid -> Range -> [Char]
-getRangeNeighbours array (Range start end) =
-    let
-        points = fmap (Point (getX start)) [getY start .. getY end]
-     in
-        mconcat $ fmap (arrayNeighbours array) points
+makeNumber :: [(Char, Int)] -> Int
+makeNumber ns = read $ fst <$> ns
+
+makePartNumber :: Int -> [(Char, Int)] -> PartNumber
+makePartNumber x ns = PartNumber (makeNumber ns) (makeRange x ns)
+
+areBothNumbers :: (Char, a) -> (Char, a) -> Bool
+areBothNumbers (x, _) (y, _) = isNumber x && isNumber y
+
+isPartNumber :: Grid -> PartNumber -> Bool
+isPartNumber array (PartNumber _ range) = any isSymbol rangeNeighbours
+  where
+    rangeNeighbours = getRangeNeighbours array range
+
+isSymbol :: Char -> Bool
+isSymbol c = (not . isNumber) c && c /= '.'
+
+-- generic array logic
+
+getPoints :: Range -> [Point]
+getPoints (Range start end) = fmap (Point x1) [y1 .. y2]
+  where
+    x1 = getX start
+    y1 = getY start
+    y2 = getY end
 
 neighbours :: Point -> [Point]
 neighbours point =
@@ -70,13 +82,21 @@ neighbours point =
     y = getY point
 
 arrayNeighbours :: Grid -> Point -> [Char]
-arrayNeighbours array (Point x y) =
-    neighbourLookup array <$> filter isInBounds (neighbours (Point x y))
+arrayNeighbours array point = arrayLookup array <$> filter (isInBounds array) (neighbours point)
+
+arrayLookup :: Grid -> Point -> Char
+arrayLookup array (Point x y) = (array !! x) !! y
+
+isInBounds :: Grid -> Point -> Bool
+isInBounds array (Point x y) = x >= 0 && x < maxX && y >= 0 && y < maxY
   where
     maxX = length array
-    maxY = length (head array)
-    neighbourLookup arr pt = (arr !! getX pt) !! getY pt
-    isInBounds pt = getX pt >= 0 && getX pt < maxX && getY pt >= 0 && getY pt < maxY
+    maxY = (length . head) array
+
+getRangeNeighbours :: Grid -> Range -> [Char]
+getRangeNeighbours array range = mconcat $ fmap (arrayNeighbours array) (getPoints range)
+
+-- utility
 
 enumerate :: [a] -> [(a, Int)]
 enumerate arr = zip arr [0 ..]
