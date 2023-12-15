b5a652471b61cbf836e91e77bc6d9174ac2ad1aa
Emilio Ziniades <emilioziniades@protonmail.com>
2023-12-15 14:10:22 +0200

    day 14 part 2 faster

--- src/Day14.hs
+++ src/Day14.hs
@@ -2,6 +2,7 @@ module Day14 (measureLoad, measureLoadWithCycles) where
 
 import Data.Bifunctor
 import Data.List
+import qualified Data.Map as Map
 import Data.Maybe
 import Util.Lists
 
@@ -20,32 +21,29 @@ measureLoad filename = do
 measureLoadWithCycles :: FilePath -> IO Int
 measureLoadWithCycles filename = do
     file <- readFile filename
-    let input = lines file
-    let directions = cycle [North .. East]
     let nCycles = 1000000000
-    pure $ totalLoad $ tiltNTimesWithCycle (nCycles * 4) input directions
+    pure $ totalLoad $ tiltNTimesWithCycle (nCycles * 4) (lines file) (cycle [North .. East])
 
 tiltNTimesWithCycle :: Int -> [String] -> [Direction] -> [String]
-tiltNTimesWithCycle n grid directions =
-    let
-        (firstRepeat, uniqueCycles) = fromJust $ uncons $ tiltUntilCycle (pure grid) directions
-        (gr, g0) = splitAt (1 + fromJust (elemIndex firstRepeat uniqueCycles)) uniqueCycles
-        n0 = length g0
-        nr = length gr
-        r = div (n - n0) nr
-        n1 = r * nr
-        n2 = mod (n - n0) nr
-        ds = take n2 $ drop ((n0 + n1) `rem` 4) directions
-     in
-        foldl tilt firstRepeat ds
-
-tiltUntilCycle :: [[String]] -> [Direction] -> [[String]]
-tiltUntilCycle grids@(g : _) (d : ds)
-    | allUnique = tiltUntilCycle (tilt g d : grids) ds
-    | otherwise = grids
+tiltNTimesWithCycle n grid directions = foldl tilt firstRepeat ds
   where
-    allUnique = length (nub grids) == length grids
-tiltUntilCycle _ _ = error "grids list should always grow"
+    (firstRepeat, uniqueCycles) = fromJust $ uncons $ tiltUntilCycle Map.empty 0 grid directions
+    (gr, g0) = splitAt (1 + fromJust (elemIndex firstRepeat uniqueCycles)) uniqueCycles
+    n0 = length g0
+    nr = length gr
+    r = div (n - n0) nr
+    n1 = r * nr
+    n2 = mod (n - n0) nr
+    ds = take n2 $ drop ((n0 + n1) `rem` 4) directions
+
+tiltUntilCycle :: Map.Map [String] Int -> Int -> [String] -> [Direction] -> [[String]]
+tiltUntilCycle cache n g (d : ds)
+    | seenBefore = newG : fmap fst (sortBy (\(_, a) (_, b) -> compare b a) $ Map.toList cache)
+    | otherwise = tiltUntilCycle (Map.insert newG n cache) (n + 1) newG ds
+  where
+    newG = tilt g d
+    seenBefore = Map.member newG cache
+tiltUntilCycle _ _ _ _ = error "grids list should always grow"
 
 -- Common to Part 1 and Part 2
 
