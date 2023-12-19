07f390d5e6fc96df29f1aa607b7862e1bddc9860
Emilio Ziniades <emilioziniades@protonmail.com>
2023-12-19 15:34:19 +0200

    day 17 add input test

--- test/Day17Test.hs
+++ test/Day17Test.hs
@@ -4,6 +4,7 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 import Day17
+import Util.Fetch
 
 day17Tests :: TestTree
 day17Tests =
@@ -14,5 +15,10 @@ day17Tests =
             [ testCase "Example" $ do
                 actual <- minimizeHeatLoss "example/day_17.txt"
                 actual @?= 102
+            , testCase "Input" $ do
+                getInput 17
+                actual <- minimizeHeatLoss "input/day_17.txt"
+                -- too low: 873, 875
+                actual @?= 0
             ]
         ]
