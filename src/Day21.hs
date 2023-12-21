5c5ec15ebabbd17e6bce526e83eb3aacc0d14396
Emilio Ziniades <emilioziniades@protonmail.com>
2023-12-21 23:28:29 +0200

    day 21 part 2 examples

--- src/Day21.hs
+++ src/Day21.hs
@@ -1,4 +1,4 @@
-module Day21 (countSteps) where
+module Day21 (countSteps, countMoreSteps) where
 
 import qualified Data.Map as Map
 import Data.Maybe
@@ -28,3 +28,31 @@ neighbours :: Garden -> Point -> [Point]
 neighbours garden (Point x y) = filter ((/= '#') . fromJust . (`Map.lookup` garden)) $ filter (`Map.member` garden) ns
   where
     ns = [Point (x + 1) y, Point (x - 1) y, Point x (y + 1), Point x (y - 1)]
+
+-- Part 2
+
+countMoreSteps :: Int -> FilePath -> IO Int
+countMoreSteps n filename = do
+    file <- readFile filename
+    let garden = gridMap (lines file)
+    let (maxX, maxY) = findMaxXAndY (lines file)
+    pure $ countNSteps' garden (maxX, maxY) n
+
+countNSteps' :: Garden -> (Int, Int) -> Int -> Int
+countNSteps' garden maxes n = Set.size $ iterate (nextSteps' garden maxes) (Set.singleton start) !! n
+  where
+    start = findStart garden
+
+nextSteps' :: Garden -> (Int, Int) -> Set.Set Point -> Set.Set Point
+nextSteps' garden maxes visited = Set.fromList $ concatMap (neighbours' garden maxes) (Set.toList visited)
+
+neighbours' :: Garden -> (Int, Int) -> Point -> [Point]
+neighbours' garden maxes (Point x y) = filter (isEmptyInInfiniteGarden garden maxes) ns
+  where
+    ns = [Point (x + 1) y, Point (x - 1) y, Point x (y + 1), Point x (y - 1)]
+
+isEmptyInInfiniteGarden :: Garden -> (Int, Int) -> Point -> Bool
+isEmptyInInfiniteGarden garden (maxX, maxY) (Point x y) = (/= '#') $ fromJust $ Map.lookup (Point (mod x maxX) (mod y maxY)) garden
+
+findMaxXAndY :: [String] -> (Int, Int)
+findMaxXAndY grid = (length grid, length (head grid))
