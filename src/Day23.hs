8286410bfa28f5768b1a02459087f51223ae5ce6
Emilio Ziniades <emilioziniades@protonmail.com>
2024-11-06 22:00:14 +0200

    day 23 tidy

--- src/Day23.hs
+++ src/Day23.hs
@@ -1,6 +1,6 @@
 module Day23 (findLongestHike) where
 
-import Data.Set (Set, insert, notMember)
+import qualified Data.Set as Set
 import GHC.Arr (Array, bounds, listArray, (!))
 import Prelude hiding (lookup)
 
@@ -12,16 +12,15 @@ data Tile = Path | Forest | SteepUp | SteepRight | SteepDown | SteepLeft
 data Position = Position {getX :: Int, getY :: Int}
     deriving (Show, Eq, Ord)
 
-data State = State {getCurrent :: Position, getVisited :: Set Position}
+data State = State {getCurrent :: Position, getVisited :: Set.Set Position}
     deriving (Show)
 
+-- Part 1
+--
 findLongestHike :: String -> Int
-findLongestHike file = longestHike grid endPosition startState
+findLongestHike file = longestHike grid (getEndPosition grid) (State (Position 0 1) Set.empty)
   where
-    grid = makeArray <$> makeArray ((fmap . fmap) newTile (lines file))
-    startPosition = Position 0 1
-    endPosition = Position (snd . bounds $ grid) (snd . bounds $ grid ! 0)
-    startState = State startPosition mempty
+    grid = parseInput file
 
 longestHike :: Grid -> Position -> State -> Int
 longestHike grid endPosition (State current@(Position x y) visited)
@@ -30,14 +29,16 @@ longestHike grid endPosition (State current@(Position x y) visited)
     | otherwise = (+ 1) $ maximum $ fmap (\s -> longestHike grid endPosition (State s nextVisited)) nextNeighbours
   where
     tile = lookup grid current
-    nextNeighbours = filter (isPath grid) $ filter (`notMember` visited) $ filter (isInGrid grid) $ case tile of
+    nextNeighbours = filter (isPath grid) $ filter (`Set.notMember` visited) $ filter (isInGrid grid) $ case tile of
         SteepUp -> [Position (x - 1) y]
         SteepRight -> [Position x (y + 1)]
         SteepDown -> [Position (x + 1) y]
         SteepLeft -> [Position x (y - 1)]
         Path -> neighbours current
         Forest -> error "current position can never be a forest"
-    nextVisited = insert current visited
+    nextVisited = Set.insert current visited
+
+-- General grid logic
 
 isPath :: Grid -> Position -> Bool
 isPath grid position = tile `elem` [Path, SteepUp, SteepRight, SteepDown, SteepLeft]
@@ -49,6 +50,22 @@ isInGrid grid (Position x y) = x >= minX && x <= maxX && y >= minY && y <= maxY
   where
     (minX, maxX) = bounds grid
     (minY, maxY) = bounds (grid ! 0)
+
+neighbours :: Position -> [Position]
+neighbours (Position x y) =
+    [ Position x (y + 1)
+    , Position x (y - 1)
+    , Position (x + 1) y
+    , Position (x - 1) y
+    ]
+
+getEndPosition :: Grid -> Position
+getEndPosition grid = Position (snd . bounds $ grid) (snd . bounds $ grid ! 0)
+
+-- Input parsing
+
+parseInput :: String -> Grid
+parseInput file = makeArray <$> makeArray ((fmap . fmap) newTile (lines file))
 
 newTile :: Char -> Tile
 newTile '.' = Path
@@ -64,11 +81,3 @@ makeArray xs = listArray (0, length xs - 1) xs
 
 lookup :: Grid -> Position -> Tile
 lookup array (Position x y) = array ! x ! y
-
-neighbours :: Position -> [Position]
-neighbours (Position x y) =
-    [ Position x (y + 1)
-    , Position x (y - 1)
-    , Position (x + 1) y
-    , Position (x - 1) y
-    ]
