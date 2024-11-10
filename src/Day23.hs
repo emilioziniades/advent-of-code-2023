7d906897bb8b728bfbb4d219356036d61892d39e
Emilio Ziniades <emilioziniades@protonmail.com>
2024-11-10 14:50:40 +0200

    day 23 part 2 example

--- src/Day23.hs
+++ src/Day23.hs
@@ -1,63 +1,59 @@
 module Day23 (findLongestHike, findLongestHikeNoSlope) where
 
+import Data.Maybe (fromJust)
 import qualified Data.Set as Set
 import GHC.Arr (Array, bounds, listArray, (!))
 import Prelude hiding (lookup)
 
+type Position = (Int, Int)
+
 type Grid = Array Int (Array Int Tile)
+
+type GetNeighbours = Grid -> Position -> [Position]
 
 data Tile = Path | Forest | SteepUp | SteepRight | SteepDown | SteepLeft
     deriving (Show, Eq)
 
-data Position = Position {getX :: Int, getY :: Int}
-    deriving (Show, Eq, Ord)
-
 data State = State {getCurrent :: Position, getVisited :: Set.Set Position}
     deriving (Show)
-
-type GetNeighbours = Grid -> Position -> [Position]
 
 -- Part 1
 
 findLongestHike :: String -> Int
-findLongestHike file = longestHike grid getNeighboursWithSlope endPosition startState
+findLongestHike file = fromJust $ longestHike grid getNeighboursWithSlope (getEndPosition grid) (State (0, 1) Set.empty)
   where
     grid = parseInput file
-    endPosition = getEndPosition grid
-    startState = State (Position 0 1) Set.empty
 
 getNeighboursWithSlope :: GetNeighbours
 getNeighboursWithSlope grid position = case tile of
-    SteepUp -> [Position (x - 1) y]
-    SteepRight -> [Position x (y + 1)]
-    SteepDown -> [Position (x + 1) y]
-    SteepLeft -> [Position x (y - 1)]
+    SteepUp -> [(x - 1, y)]
+    SteepRight -> [(x, y + 1)]
+    SteepDown -> [(x + 1, y)]
+    SteepLeft -> [(x, y - 1)]
     Path -> neighbours position
     Forest -> error "current position can never be a forest"
   where
     tile = lookup grid position
-    x = getX position
-    y = getY position
+    x = fst position
+    y = snd position
 
 -- Part 2
 
 findLongestHikeNoSlope :: String -> Int
-findLongestHikeNoSlope file = longestHike grid getNeighboursNoSlope endPosition startState
+findLongestHikeNoSlope file = fromJust $ longestHike grid getNeighboursNoSlope (getEndPosition grid) (State (0, 1) Set.empty)
   where
     grid = parseInput file
-    endPosition = getEndPosition grid
-    startState = State (Position 0 1) Set.empty
 
 getNeighboursNoSlope :: GetNeighbours
 getNeighboursNoSlope _ = neighbours
 
 -- Common to Part 1 and 2
 
-longestHike :: Grid -> GetNeighbours -> Position -> State -> Int
+longestHike :: Grid -> GetNeighbours -> Position -> State -> Maybe Int
 longestHike grid getNeighbours endPosition (State position visited)
-    | position == endPosition = 0
-    | null nextNeighbours = 0 -- TODO: this allows illegal states, figure out how to abort calculation if dead-end reached
-    | otherwise = (+ 1) $ maximum $ fmap nextStates nextNeighbours
+    | position == endPosition = Just 0
+    | null nextNeighbours = Nothing
+    | otherwise = ((+) <$> Just 1 <*>) $ maximum $ fmap nextStates nextNeighbours
   where
     nextNeighbours = filter (isPath grid) $ filter (`Set.notMember` visited) $ filter (isInGrid grid) (getNeighbours grid position)
     nextVisited = Set.insert position visited
@@ -66,7 +62,7 @@ longestHike grid getNeighbours endPosition (State position visited)
 -- General grid logic
 
 lookup :: Grid -> Position -> Tile
-lookup array (Position x y) = array ! x ! y
+lookup array (x, y) = array ! x ! y
 
 isPath :: Grid -> Position -> Bool
 isPath grid position = tile `elem` [Path, SteepUp, SteepRight, SteepDown, SteepLeft]
@@ -74,21 +70,17 @@ isPath grid position = tile `elem` [Path, SteepUp, SteepRight, SteepDown, SteepL
     tile = lookup grid position
 
 isInGrid :: Grid -> Position -> Bool
-isInGrid grid (Position x y) = x >= minX && x <= maxX && y >= minY && y <= maxY
+isInGrid grid (x, y) = x >= minX && x <= maxX && y >= minY && y <= maxY
   where
     (minX, maxX) = bounds grid
     (minY, maxY) = bounds (grid ! 0)
 
 neighbours :: Position -> [Position]
-neighbours (Position x y) =
-    [ Position x (y + 1)
-    , Position x (y - 1)
-    , Position (x + 1) y
-    , Position (x - 1) y
-    ]
+neighbours (x, y) = [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]
 
+-- assumes the end position is always one tile left of the bottom right corner
 getEndPosition :: Grid -> Position
-getEndPosition grid = Position (snd . bounds $ grid) (snd . bounds $ grid ! 0)
+getEndPosition grid = (snd . bounds $ grid, subtract 1 . snd . bounds $ grid ! 0)
 
 -- Input parsing
 
